8b0ba4db46f87507b0af832d32a5df1ee2f92571
Stay1444 <staylluc@gmail.com>
2021-11-21 22:02:58 +0100

    ADDED FILE

---
+++ StayNet/Server/Entities/ClientConnectionData.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text;
+
+namespace StayNet.Server.Entities
+{
+    public sealed class ClientConnectionData
+    {
+        public MemoryStream Stream { get; private set; }
+        
+        public void Read(byte[] buffer, int offset, int count)
+        {
+            Stream.Read(buffer, offset, count);
+        }
+        
+        public string ReadString()
+        {
+            //convert all memoryStream to a UTF-8 string
+            byte[] bytes = Stream.ToArray();
+            return Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+        }
+    }
+}
---
+++ StayNet/Server/Events/ClientConnectedEvent.cs
@@ -0,0 +1,16 @@
+﻿using StayNet.Server.Entities;
+
+namespace StayNet.Server.Events
+{
+    public class ClientConnectedEvent : ServerNetEvent
+    {
+        public ClientConnectedEvent(Client client, ClientConnectionData connectionData)
+        {
+            Client = client;
+            ConnectionData = connectionData;
+        }
+
+        public Client Client { get; private set; }
+        public ClientConnectionData ConnectionData { get; private set; }
+    }
+}
