6c6ac503b9b79c7c63311fa5cc6b5a4db1677c51
Stay1444 <staylluc@gmail.com>
2021-11-22 22:15:23 +0100

    added method attribute

---
+++ StayNet/Common/Attributes/Method.cs
@@ -0,0 +1,14 @@
+﻿namespace StayNet.Common.Attributes
+{
+    
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class Method : Attribute
+    {
+        public String MethodId { get; private set; }
+        
+        public Method(string Method)
+        {
+            MethodId = Method;
+        }
+    }
+}
--- StayNet/Server/Controllers/BaseController.cs
+++ StayNet/Server/Controllers/BaseController.cs
@@ -1,9 +1,17 @@
-﻿namespace StayNet.Server.Controllers
+﻿using System.Linq;
+using System.Threading.Tasks;
+using StayNet.Common.Attributes;
+
+namespace StayNet.Server.Controllers
 {
     public abstract class BaseController
     {
-
         
+        public virtual async Task BeforeMethodInvoke(object context)
+        {
+            await Task.CompletedTask;
+        }
+
         
     }
 }
